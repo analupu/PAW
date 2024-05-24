48e370ac339f3e63cb2efde11a3a191d98c626c3
analupu <146833178+analupu@users.noreply.github.com>
2024-05-24 14:27:18 +0300

    Rezervari Hotel

---
+++ Rezervari Hotel/Form2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+
+namespace Rezervari_Hotel
+{
+    public partial class Form2 : Form
+    {
+        private  string BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private  string ConnectionString;
+        private string FilePath;
+        private turisti turist;
+        public Form2 (List<turisti> turisti)
+        {
+            InitializeComponent ();
+            
+
+            ConnectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Path.Combine(BaseDirectory, "turisti.accdb")}";
+            FilePath = Path.Combine(BaseDirectory, "turisti.txt");
+            LoadDataFromDatabase();
+            dataGridViewRerervari.CellClick += dataGridViewRerervari_CellContentClick;
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+            this.FormClosed += new FormClosedEventHandler(Form2_FormClosed);
+            ConnectionString = $@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Path.Combine(BaseDirectory, "turisti.accdb")}";
+            FilePath = Path.Combine(BaseDirectory, "turisti.txt");
+            dataGridViewRerervari.CellContentClick += dataGridViewRerervari_CellContentClick;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+            private void LoadDataFromFile()
+        {
+            var turistiList = new List<turisti>();
+            if (File.Exists(FilePath))
+            {
+                var lines = File.ReadAllLines(FilePath);
+                foreach (var line in lines)
+                {
+                    var data = line.Split(',');
+                    var turist = new turisti
+                    {
+                        Nume = data[0],
+                        Prenume = data[1],
+                        Varsta = int.Parse(data[2]),
+                        Domiciliu = data[3]
+                    };
+                    turistiList.Add(turist);
+                }
+            }
+            dataGridViewRerervari.DataSource = turistiList;
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            using (var connection = new OleDbConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var adapter = new OleDbDataAdapter("SELECT * FROM TURISTI", connection);
+                var dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                dataGridViewRerervari.DataSource = dataTable;
+            }
+        }
+
+       
+        private void NUMARTURISTIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateTuristiCount();
+        }
+
+        private void UpdateTuristiCount()
+        {
+
+            int numarTuristiBazaDate = 0;
+            using (var connection = new OleDbConnection(ConnectionString))
+            {
+                connection.Open();
+                var command = new OleDbCommand("SELECT COUNT(*) FROM TURISTI", connection);
+                numarTuristiBazaDate = (int)command.ExecuteScalar();
+            }
+
+            MessageBox.Show($"Numărul total de turiști din baza de date este: {numarTuristiBazaDate}", "Numar Turisti");
+        }
+
+
+
+        private void AFISEAZATURISTIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadDataFromDatabase();
+        }
+
+        private void iESIREToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridViewRerervari_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewRerervari.Rows[e.RowIndex];
+
+                string nume = row.Cells["Nume"].Value.ToString();
+                string prenume = row.Cells["Prenume"].Value.ToString();
+                int varsta = Convert.ToInt32(row.Cells["Varsta"].Value);
+                string domiciliu = row.Cells["Domiciliu"].Value.ToString();
+
+
+                Form3 form3 = new Form3(nume, prenume, varsta, domiciliu);
+                form3.ShowDialog();
+            }
+        }
+
+        private void aNULEAZAREZERVAREToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewRerervari.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridViewRerervari.SelectedRows[0];
+                int id = Convert.ToInt32(selectedRow.Cells["ID"].Value); 
+                DeleteRowFromDatabase(id);
+                dataGridViewRerervari.Rows.Remove(selectedRow);
+
+            }
+            else
+            {
+                MessageBox.Show("Selectați un rând pentru a șterge.");
+            }
+        }
+
+        private void DeleteRowFromDatabase(int id)
+        {
+            using (var connection = new OleDbConnection(ConnectionString))
+            {
+                connection.Open();
+                using (var command = new OleDbCommand("DELETE FROM TURISTI WHERE ID = @ID", connection))
+                {
+                    command.Parameters.AddWithValue("@ID", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
---
+++ Rezervari Hotel/Form4.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rezervari_Hotel
+{
+    public partial class Form4 : Form
+    {
+        private Panel colorPanel;
+        public Form4()
+        {
+            InitializeComponent();
+            InitializeTrackBar();
+            InitializeColorPanel();
+        }
+
+        private void InitializeTrackBar()
+        {
+            TrackBar trackBar = new TrackBar
+            {
+                Minimum = 0,
+                Maximum = 100,
+                TickFrequency = 10,
+                LargeChange = 10,
+                SmallChange = 1,
+                Width = 400,
+                Location = new Point((this.ClientSize.Width - 400) / 2, 50),
+                Anchor = AnchorStyles.Top | AnchorStyles.Left
+            };
+
+            Label label = new Label
+            {
+                Text = "Nivelul de satisfacție: 0%",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Width = 400,
+                Location = new Point((this.ClientSize.Width - 400) / 2, 20),
+                Anchor = AnchorStyles.Top | AnchorStyles.Left
+            };
+
+            trackBar.Scroll += (sender, e) =>
+            {
+                int value = trackBar.Value;
+                label.Text = $"Nivelul de satisfacție: {value}%";
+                colorPanel.BackColor = GetColorFromValue(value);
+            };
+
+            this.Controls.Add(trackBar);
+            this.Controls.Add(label);
+        }
+
+        private void InitializeColorPanel()
+        {
+            colorPanel = new Panel
+            {
+                Size = new Size(400, 200),
+                Location = new Point((this.ClientSize.Width - 400) / 2, 150),
+                BackColor = GetColorFromValue(0),
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            this.Controls.Add(colorPanel);
+
+            this.Resize += (sender, e) =>
+            {
+                colorPanel.Location = new Point((this.ClientSize.Width - colorPanel.Width) / 2, 150);
+            };
+        }
+
+        private Color GetColorFromValue(int value)
+        {
+            int red = (int)(255 - (value * 2.55));
+            int green = (int)(value * 2.55);
+            int blue = 0; 
+            return Color.FromArgb(red, green, blue);
+        }
+
+        public void AfiseazaInformatii(string informatii)
+        {
+            richTextBox1.Text = informatii;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
